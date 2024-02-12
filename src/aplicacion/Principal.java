4273b450e7ee06380287bf0edd22e0c5167b9f7a
enzohuin <133805188+enzohuin@users.noreply.github.com>
2024-02-12 11:06:40 +0100

    Update Principal.java

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -2,10 +2,31 @@ package aplicacion;
 
 import mates.Matematicas;
 
-public class Principal{
-    public static void main(String[] args){
-        System.out.println("El número PI es " +
-                Matematicas.generarNumeroPiIterativo(Integer.parseInt(args[0])));
+/**
+ * Clase principal que contiene el método main para ejecutar la aplicación.
+ */
+public class Principal {
+
+    /**
+     * Método principal que se ejecuta al iniciar la aplicación.
+     *
+     * @param args Los argumentos de la línea de comandos. Se espera que el primer
+     *             argumento sea el número de pasos para la aproximación de Pi.
+     */
+    public static void main(String[] args) {
+        // Verifica si se proporciona al menos un argumento en la línea de comandos.
+        if (args.length < 1) {
+            System.err.println("Se espera al menos un argumento: el número de pasos.");
+            System.exit(1);
+        }
+
+        // Convierte el primer argumento a un entero que representa el número de pasos.
+        int numeroPasos = Integer.parseInt(args[0]);
+
+        // Utiliza el método de la clase Matematicas para generar una aproximación de Pi.
+        double piAproximado = Matematicas.generarNumeroPiIterativo(numeroPasos);
+
+        // Imprime el resultado.
+        System.out.println("El número PI es " + piAproximado);
     }
 }
-
